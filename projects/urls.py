290559f74e905318f876ee4bc3cfa9e0c1e96091
danknjoroge <dank8505@gmail.com>
2022-04-06 23:51:45 +0300

    created registration routes

--- projects/urls.py
+++ projects/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django_registration.backends.one_step.views import RegistrationView
 from . import views
+
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('accounts/register/',
+        RegistrationView.as_view(success_url='/index/'),
+        name='django_registration_register'),
+    path('accounts/', include('django_registration.backends.activation.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
