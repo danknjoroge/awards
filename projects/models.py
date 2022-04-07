30cd8a8bda2a3bcdb249c76bfc0620670e4d1919
danknjoroge <dank8505@gmail.com>
2022-04-07 11:44:50 +0300

    added projects model

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,21 @@
+from email.policy import default
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+# class Profile(models.Model):
+#     user = models.OneToOneField(User,on_delete=models.CASCADE)
+#     profile_pic = models.ImageField(default='default.jpg', upload_to='images/')
+
+#     def __str__(self):
+#         return f'{self.user.username} Profile'
+
+class Projects(models.Model):
+    title = models.CharField(max_length=122)
+    image = models.ImageField(default='default.jpg', upload_to='images/')
+    description = models.TextField()
+    live_link = models.URLField(max_length=222, default='url')
+    posted_by = models.ForeignKey(User, related_name='user', on_delete=models.CASCADE )
+
+
+
