9264c96ae7b60cfe485741e90a2eeffe0ebbb81c
danknjoroge <dank8505@gmail.com>
2022-04-07 17:41:12 +0300

    created a meta class

--- projects/forms.py
+++ projects/forms.py
@@ -1,1 +1,10 @@
 from .models import Projects
+from django import forms
+
+
+class PostProject(forms.ModelForm):
+    class Meta:
+        model = Projects
+        exclude = ['posted_by', 'date_posted']
+
+        
