82a5959408944ba9da958a25f64c98e1813d07c5
danknjoroge <dank8505@gmail.com>
2022-04-07 22:06:09 +0300

    modified url paths

--- awards/settings.py
+++ awards/settings.py
@@ -132,8 +132,8 @@ MEDIA_URL = '/media/' # Public URL at the browser
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = "login"
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = "index"
 
 
 
--- projects/urls.py
+++ projects/urls.py
@@ -9,10 +9,12 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('accounts/register/',
-        RegistrationView.as_view(success_url='/'),
+        RegistrationView.as_view(success_url='/home'),
         name='django_registration_register'),
     path('accounts/', include('django_registration.backends.one_step.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     re_path(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'), 
     re_path(r'^login/$', LoginView.as_view(), {"next_page": '/'}),
+
+    path('home/', views.home, name='home'),
 ]
