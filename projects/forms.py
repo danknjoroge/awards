0edbcccb3a4a28cc95289d40b94f003ca5165809
danknjoroge <dank8505@gmail.com>
2022-04-07 17:50:22 +0300

    created a projectpost form

--- projects/forms.py
+++ projects/forms.py
@@ -2,9 +2,9 @@ from .models import Projects
 from django import forms
 
 
-class PostProject(forms.ModelForm):
+class PostProjectForm(forms.ModelForm):
     class Meta:
         model = Projects
         exclude = ['posted_by', 'date_posted']
 
-        
+
--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
+from .forms import PostProjectForm
 from .models import Projects
 
 # Create your views here.
 def index(request):
     projects = Projects.objects.all()
     return render(request, 'index.html', {"projects": projects})
+
+@login_required(login_url='/accounts/login/')
+def newProject(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = PostProjectForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.posted_by = current_user
+            post.save()
+        return redirect(index)
+
+    else:
+        form = PostProjectForm()
+    return render(request, 'index.html', {'form': form})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
