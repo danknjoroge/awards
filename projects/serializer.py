fe0442f99bccfa6a34f2d22f1538e40ed0f1eb08
danknjoroge <dank8505@gmail.com>
2022-04-08 19:02:57 +0300

    added project details

--- projects/serializer.py
+++ projects/serializer.py
@@ -4,4 +4,7 @@ from .models import Projects, User
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Projects
-        fields = ('title', 'image', 'description', 'live_link', 'posted_by', 'date_posted' )
+        fields = ('title', 'image', 'description', 'live_link', 'posted_by', 'date_posted')
+
+
+        
